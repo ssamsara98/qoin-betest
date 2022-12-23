4a715f698469405dbc8510bce8ff5229c1d5ca6d
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2022-12-23 16:52:53 +0700

    Get input from console

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # qoin-betest
 
 ## How to run
-- go run -N `<number>` -M `<number>`
-  - N : `number of players`
-  - M : `number of dice each player has`
+
+- `go run ./main.go`
+  or
+- `go build` => `./qoin-dadu`
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"qoin-dadu/src"
 )
@@ -12,17 +11,43 @@ func RemoveIndex(s []int, index int) []int {
 
 func main() {
 
-	var N = flag.Int64("N", 2, "type your N")
-	var M = flag.Int64("M", 2, "type your M")
-	flag.Parse()
+	// var N = flag.Int64("N", 2, "type your N")
+	// var M = flag.Int64("M", 2, "type your M")
+	// flag.Parse()
 
-	fmt.Printf("Pemain = %d, Dadu = %d\n", *N, *M)
+	var N int
+	var M int
+
+	fmt.Printf("Masukan jumlah pemain: ")
+	_, err := fmt.Scanln(&N)
+	if err != nil {
+		fmt.Println("Input salah.")
+		return
+	}
+	if N < 2 {
+		fmt.Println("Jumlah pemain tidak boleh kurang dari 2")
+		return
+	}
+
+	fmt.Printf("Masukan jumlah dadu: ")
+	_, err = fmt.Scanln(&M)
+	if err != nil {
+		fmt.Println("Input salah.")
+		return
+	}
+	if M < 1 {
+		fmt.Println("Jumlah pemain tidak boleh kurang dari 1")
+		return
+	}
+
+	fmt.Println()
+	fmt.Printf("Pemain = %d, Dadu = %d\n", N, M)
 
 	// players := make([]src.Player, *N)
 	players := make([]src.Player, 0)
 
-	for i := 0; int64(i) < *N; i++ {
-		player := src.Player{Dice: make([]int, *M)}
+	for i := 0; i < N; i++ {
+		player := src.Player{Dice: make([]int, M)}
 		players = append(players, player)
 	}
 
